2b36f119995e913fe5b148002fd3a857e7c8fb9a
Nikhil Pathak <nikhilpathak926@gmail.com>
2022-09-27 17:41:03 +0530

    User register command support

--- golang/journal/cmd/register.go
+++ golang/journal/cmd/register.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+	"journal/database"
 	"fmt"
 	"github.com/spf13/cobra"
 )
@@ -9,26 +10,30 @@ var registerCmd = &cobra.Command{
 	Use:   "register",
 	Short: "Register new user",
 	Long: `This command register a new user to the journal CLI app.
-For example: ./journal register --username=<username> --name=<name> --password=<password>`,
+For example:
+./journal register --username=<username> --name=<name>
+./journal register -u=<username> -n=<name>
+`,
 	Run: func(cmd *cobra.Command, args []string) {
-		if(len(args) < 1) {
-			fmt.Println("Required flags not present: ", args)
+		fmt.Println("Registering you in the system. Should be done really quick....")
+		_, err := database.CreateUser(username, name)
+		if err == nil {
+			fmt.Println("Regsitered successfully!")
+		} else {
+			fmt.Println("Failed to regsitered '%s'", err)
 		}
 	},
 }
 
 var username string
 var name string
-var password string
 
 func init() {
 	rootCmd.AddCommand(registerCmd)
 
 	registerCmd.Flags().StringVarP(&username, "username", "u", "", "Username for the user")
 	registerCmd.Flags().StringVarP(&name, "name", "n", "", "Name of the user")
-	registerCmd.Flags().StringVarP(&password, "password", "p", "", "Password for the CLI app")
 
 	registerCmd.MarkFlagRequired("username")	
 	registerCmd.MarkFlagRequired("name")
-	registerCmd.MarkFlagRequired("password")
 }
--- golang/journal/database/db.go
+++ golang/journal/database/db.go
@@ -1,24 +1,74 @@
-pacakge database
+package database
 
 import(
 	"encoding/json"
+	"fmt"
 	"os"
-	"strings"
 )
 
+const DB_FILENAME = "database/db.json"
+
 type User struct {
-	username string
-	name string
-	password string
+	Username string
+	Name string
 }
 
-func CreateUser(username string, name string, password string) *User {
-	newUser := User{
-		username: username,
-		name: name,
-		password: password,	
+func getUsers() ([]User, error) {
+	data, err := os.ReadFile(DB_FILENAME)
+	var users []User
+
+	if err == nil {
+		json.Unmarshal(data, &users)
+	}
+	
+	return users, err
+}
+
+func FindUser(username string) (*User, error) {
+	users, err := getUsers()
+
+	if(err == nil) {
+		for index := 0; index < len(users); index++ {
+			user := users[index]
+
+			if(user.Username == username) {
+				return &user, nil
+			}
+		}
 	}
 
-	return newUser
+	return nil, err
+}
+
+func updateDB(data []User) {
+	bytes, err := json.Marshal(data)
+		
+	if err == nil {
+		os.WriteFile(DB_FILENAME, bytes, 0644)
+	} else {
+		fmt.Println("Failed to save to DB file '%s'", err)
+	}
+}
+
+func CreateUser(username string, name string) (*User, error) {
+	user, err := FindUser(username)
+
+	if user != nil {
+		return user, err
+	}
+	
+	newUser := User{
+		Username: username,
+		Name: name,
+	}
+
+	users, err := getUsers()
+	
+	if err == nil {
+		users = append(users, newUser)
+		updateDB(users)
+	}
+
+	return &newUser, err
 }
 
