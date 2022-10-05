326da0013986f7df41745bcb8486882eb9fbda30
Nikhil Pathak <nikhilpathak926@gmail.com>
2022-10-05 20:47:03 +0530

    Database setup

---
+++ golang/e-commerce/database/db.go
@@ -0,0 +1,22 @@
+package database
+
+import(
+	"database/sql"
+	"log"
+	_ "github.com/lib/pq"
+)
+
+func OpenDbConnection() *sql.DB {
+	db, err := sql.Open("postgres", "postgresql://postgres:postgres@localhost:5433/e_commerce?sslmode=disable")
+	if err != nil {
+		log.Fatalf("could not connect to database: %v", err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatalf("unable to reach database: %v", err)
+	}
+
+	return db
+}
+
