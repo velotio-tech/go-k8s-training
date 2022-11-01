e9e560d4a72d440461b0dfbfdfaac81ab8d08a19
jshiwam <jaiswalshiwam369@gmail.com>
2022-11-01 16:45:52 +0530

    Completed Assignment 1

--- golang/Assignment1/main.go
+++ golang/Assignment1/main.go
@@ -3,39 +3,94 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io/ioutil"
 	"os"
 	"os/exec"
 	"os/user"
 	"strings"
 )
 
+// Commands
 const (
-	ls   string = "ls"
-	pwd         = "pwd"
-	cd          = "cd"
-	exit        = "exit"
-	help        = "help"
+	ls      string = "ls"
+	pwd            = "pwd"
+	cd             = "cd"
+	exit           = "exit"
+	help           = "help"
+	history        = "history"
 )
 
-// Tried to use factory pattern but didn't implement it, which would require me to
+func getHomedir() string {
+	home, err := os.UserHomeDir()
+	if err != nil {
+		home, err = os.Getwd()
+		if err != nil {
+			panic(err.Error())
+		}
+	}
+	return home
+}
+
+var HOME = getHomedir()
+
+// History Filename
+const (
+	HISTORY_FILE = "/.gohistory"
+)
+
+// Create file that saves history command during the startup
+func init() {
+	createHistoryFile()
+}
+
+func createHistoryFile() {
+	_, err := os.Create(HOME + HISTORY_FILE)
+	if err != nil {
+		fmt.Println("Cannot create history file", err.Error())
+	}
+}
+
+func readHistory() (string, error) {
+	history, err := ioutil.ReadFile(HOME + HISTORY_FILE)
+	if err != nil {
+		return "", err
+	}
+	return string(history), nil
+}
+
+func updateHistory(cmd string) {
+	f, err := os.OpenFile(HOME+HISTORY_FILE, os.O_APPEND|os.O_WRONLY, 0644)
+	if err != nil {
+		fmt.Println("Can't update History", err.Error())
+		return
+	}
+	defer f.Close()
+	if _, err := f.WriteString(cmd + "\n"); err != nil {
+		fmt.Println("Can't update History", err.Error())
+		return
+	}
+}
+
+// Interface with common methods for each command.
 type Command interface {
+	// Checks if the command is valid.
 	IsValid() bool
+	// Executes the command.
 	Execute() (string, error)
 }
 
+// Command specific structs
 type Ls struct{ words []string }
 type Pwd struct{ words []string }
 type Cd struct{ words []string }
 type Exit struct{ words []string }
+type History struct{ words []string }
 type Help struct{ words []string }
 
 func (l Ls) IsValid() bool {
 	return len(l.words) == 1 && l.words[0] == ls
 }
 
-// 	out, err := exec.Command("ls").Output()
-// 	out2, err := exec.Command("pwd").Output()
-// 	out3, err := exec.Command("cd", "../").Output()
 func (l Ls) Execute() (string, error) {
 	out, err := exec.Command(l.words[0]).Output()
 	return string(out), err
@@ -54,8 +109,8 @@ func (c Cd) IsValid() bool {
 	return len(c.words) == 2 && c.words[0] == cd
 }
 func (c Cd) Execute() (string, error) {
-	out, err := exec.Command("bash", "-c", fmt.Sprintf("%s %s", c.words[0], c.words[1])).Output()
-	return string(out), err
+	err := os.Chdir(c.words[1])
+	return "", err
 }
 
 func (e Exit) IsValid() bool {
@@ -79,14 +134,25 @@ func (h Help) Execute() (string, error) {
 	return helpstr, nil
 }
 
+func (h History) IsValid() bool {
+	return len(h.words) == 1 && h.words[0] == history
+}
+
+func (h History) Execute() (string, error) {
+	return readHistory()
+}
+
+// Function that takes Command Interface and calls the specific IsValid method.
 func isValid(cmd Command) bool {
 	return cmd.IsValid()
 }
 
+// Function that takes Command Interface and calls the specific Execute method.
 func execute(cmd Command) (string, error) {
 	return cmd.Execute()
 }
 
+// Factory funtion that returns Command Struct based on Command type
 func CommandFactory(words []string) Command {
 	if words[0] == ls {
 		return Ls{words: words}
@@ -98,6 +164,8 @@ func CommandFactory(words []string) Command {
 		return Cd{words: words}
 	} else if words[0] == help {
 		return Help{words: words}
+	} else if words[0] == history {
+		return History{words: words}
 	}
 	return nil
 }
@@ -105,6 +173,7 @@ func CommandFactory(words []string) Command {
 func main() {
 	fmt.Println("Go CLI started")
 	scanner := bufio.NewScanner(os.Stdin)
+
 	for i := ""; i != exit; {
 		user, err := user.Current()
 		if err != nil {
@@ -129,11 +198,12 @@ func main() {
 			continue
 		}
 		in := scanner.Text()
+		updateHistory(in)
 		words := strings.Fields(in)
 		var cmd Command
 		if len(words) > 0 {
 			cmd = CommandFactory(words)
-			if isValid(cmd) {
+			if cmd != nil && isValid(cmd) {
 				i, err = execute(cmd)
 				if err != nil {
 					fmt.Println(err.Error())
