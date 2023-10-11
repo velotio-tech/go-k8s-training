fa755994ca9afdcd205b923a6553d3fb01baafdd
shivansh-pandeyy <shivansh.pandey@velotio.com>
2023-10-11 22:47:20 +0530

    go-assignment-2

---
+++ journal/utils/encrypt.go
@@ -0,0 +1,57 @@
+package utils
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"encoding/base64"
+)
+
+var bytes = []byte{35, 46, 57, 24, 85, 35, 24, 74, 87, 35, 88, 98, 66, 32, 14, 05}
+const key string = "abc&1*~#^2^#s0^=)^^7%b34"
+
+
+func Encode(b []byte) string {
+	return base64.StdEncoding.EncodeToString(b)
+}
+
+func Decode(s string) []byte {
+	data, err := base64.StdEncoding.DecodeString(s)
+
+	if err != nil {
+		panic(err)
+	}
+
+	return data
+}
+
+// Encrypt method is to encrypt text
+func Encrypt(text string) (string, error) {
+	block, err := aes.NewCipher([]byte(key))
+
+	if err != nil {
+		return "", err
+	}
+
+	plainText := []byte(text)
+	cfb := cipher.NewCFBEncrypter(block, bytes)
+	cipherText := make([]byte, len(plainText))
+	cfb.XORKeyStream(cipherText, plainText)
+
+	return Encode(cipherText), nil
+}
+
+// Decrypt method is to extract back the encrypted text
+func Decrypt(text string) (string, error) {
+	block, err := aes.NewCipher([]byte(key))
+
+	if err != nil {
+		return "", err
+	}
+
+	cipherText := Decode(text)
+	cfb := cipher.NewCFBDecrypter(block, bytes)
+	plainText := make([]byte, len(cipherText))
+	cfb.XORKeyStream(plainText, cipherText)
+
+	return string(plainText), nil
+}
